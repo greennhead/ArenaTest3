3258836da3f590e812e5be01a2ae119453f267a0
greennhead <102859098+greennhead@users.noreply.github.com>
2026-07-08 21:56:20 +0500

    ahaha

--- nodes/palleterizer.gd
+++ nodes/palleterizer.gd
@@ -6,5 +6,5 @@ func set_palette(palette : Texture2D):
 		$ColorRect.material.set("shader_parameter/palette",load("res://images/palette.png"))
 		return
 	var pal = palette.get_image()
-	pal.resize(clamp(pal.get_width(),1,48),clamp(pal.get_height(),1,32))
+	pal.resize(clamp(pal.get_width(),1,24),clamp(pal.get_height(),1,24))
 	$ColorRect.material.set("shader_parameter/palette",ImageTexture.create_from_image(pal))
